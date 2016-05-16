3d0d3107c4bbe27c1dea53c51d04282abc0f6d4c
Alexander Kleinschmidt <alex@kungf.ooo>
2016-05-16 18:03:47 +0200

    new List  model, and migratation S. 101

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 from django.template.loader import render_to_string
 
@@ -61,16 +61,24 @@ class HomePageTest(TestCase):
         # TODO more test
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'the first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'second list item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -78,7 +86,10 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'the first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'second list item')
+        self.assertEqual(second_saved_item.list, list_)
+
 
 
 class ListViewTest(TestCase):
@@ -88,8 +99,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_items(self):
-        Item.objects.create(text='itmem1')
-        Item.objects.create(text='itmem2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itmem1', list=list_)
+        Item.objects.create(text='itmem2', list=list_)
 
         response = self.client.get('/lists/the-only-list/')
 
