9f77385e46ff18b6b3eaa7d706e9ab631ad25ea3
Alexander Kleinschmidt <alex@kungf.ooo>
2016-05-18 01:54:13 +0200

    Moved FTs into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,21 @@
+from unittest import skip
+from .base import FunctionalTest
+
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_lists_items(self):
+        # submit an empty item
+
+        # there is an error massage
+
+        # add a not emtpy item
+
+        # submit an empty item
+
+        # there is an error massage on the list page
+
+        # And She can correct them
+
+        self.fail('write me!')
+
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,38 +1,9 @@
+from .base import FunctionalTest
 from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
-import sys
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://'+arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    # eigene Helper Methode
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows], "New to-do item not found in your list")
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -48,7 +19,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
-        list_url=self.browser.current_url
+        list_url = self.browser.current_url
         self.assertRegex(list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -62,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # new user
         self.browser.quit()
-        self.browser =webdriver.Firefox()
+        self.browser = webdriver.Firefox()
         self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
@@ -73,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         new_list_url = self.browser.current_url
-        self.assertRegex( new_list_url, '/lists/.+')
+        self.assertRegex(new_list_url, '/lists/.+')
         self.assertNotEqual(new_list_url, list_url)
 
         # again
@@ -83,16 +54,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Milch', page_text)
 
         # reminder self.fail('Finish the test!')
-
-    def test_layout_and_styling(self):
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
-
-        inputbox.send_keys('testing\n')
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
 
