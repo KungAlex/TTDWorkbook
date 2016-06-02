52b44cc1110b374d9228d8792ab85720343fb64e
Alexander Kleinschmidt <alex@kungf.ooo>
2016-06-02 02:18:29 +0200

    FT for error massages cleared automaticly s. 226

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,13 +4,16 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_lists_items(self):
         # submit an empty item
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
 
         # there is an error massage
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # add a not emtpy item
@@ -22,7 +25,7 @@ class ItemValidationTest(FunctionalTest):
 
         # there is an error massage on the list page
         self.check_for_row_in_list_table('1: Milch')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And She can correct them
@@ -40,5 +43,16 @@ class ItemValidationTest(FunctionalTest):
         # tries to enter a duplicate
         self.get_item_input_box().send_keys('Buy wellies\n')
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # clear the error
+        self.get_item_input_box().send_keys('a')
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
